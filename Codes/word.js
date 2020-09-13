d019d4fac75a2554bec4034e93ad578bc3121b89
Pramod <kotapramod8@gmail.com>
2020-09-13 15:21:09 +0530

    words are added

---
+++ Codes/word.js
@@ -0,0 +1,102 @@
+var sentence1_1=["a.John ate an apple before afternoon"];
+var sentence1_2=["b.before afternoon John ate an apple"];	
+var sentence1_3=["c.John before afternoon ate an apple"];
+var sentence2_1=["a.some students like to study in the night"];
+var sentence2_2=["b.at night some students like to study"];
+var sentence3_1=["a.John and Mary went to church"];
+var sentence3_2=["b.Mary and John went to church"];
+var sentence4_1=["a.John went to church after eating"];	
+var sentence4_2=["b.after eating John went to church"];
+var sentence4_3=["c.John after eating went to church"];
+var sentence5_1=["a.did he go to market"];
+var sentence5_2=["b.he did go to market"];
+var sentence6_1=["a.the woman who called my sister sells cosmetics"];
+var sentence6_2=["b.the woman who sells cosmetics called my sister"];
+var sentence6_3=["c.my sister who sells cosmetics called the woman"];
+var sentence6_4=["d.my sister who called the woman sells cosmetics"];
+var sentence7_1=["a.John goes to the library and studies"];
+var sentence7_2=["b.John studies and goes to the library"];
+var sentence8_1=["a.John ate an apple so did she"];
+var sentence8_2=["b.she ate an apple so did John"];
+var sentence9_1=["a.the teacher returned the book after she noticed the error"];
+var sentence9_2=["b.the teacher noticed the error after she returned the book"];
+var sentence9_3=["c.after the teacher returned the book she noticed the error"];
+var sentence9_4=["d.after the teacher noticed the error she returned the book"];
+var sentence9_5=["e.she returned the book after the teacher noticed the error"];
+var sentence9_6=["f.she noticed the error after the teacher returned the book"];
+var sentence9_7=["g.after she returned the book the teacher noticed the error"];
+var sentence9_8=["h.after she noticed the error the teacher returned the book"];
+var sentence10_1=["a.I told her that I bought a book yesterday"];
+var sentence10_2=["b.I told her yesterday that I bought a book"];
+var sentence10_3=["c.yesterday I told her that I bought a book"];
+var sentence10_4=["d.I bought a book that I told her yesterday"];
+var sentence10_5=["e.I bought a book yesterday that I told her"];	
+var sentence10_6=["f.yesterday I bought a book that I told her"];
+
+var word1_1=["a.राम और श्याम बाजार गयें"];
+var word1_2=["b.राम और श्याम गयें बाजार"];
+var word1_3=["c.बाजार गयें राम और श्याम"];
+var word1_4=["d.गयें बाजार राम और श्याम"];
+var word2_1=["a.राम सोया और श्याम भी"];
+var word2_2=["b.श्याम सोया और राम भी"];
+var word2_3=["c.सोया श्याम और राम भी"];
+var word2_4=["d.सोया राम और श्याम भी"];
+var word3_1=["a.मैंने उसे बताया कि राम सो रहा है"];
+var word3_2=["b.मैंने उसे बताया कि सो रहा है राम"];
+var word3_3=["c.उसे मैंने बताया कि राम सो रहा है"];
+var word3_4=["d.उसे मैंने बताया कि सो रहा है राम"];
+var word3_5=["e.मैंने बताया उसे कि राम सो रहा है"];
+var word3_6=["f.मैंने बताया उसे कि सो रहा है राम"];
+var word3_7=["g.उसे बताया मैंने कि राम सो रहा है"];
+var word3_8=["h.उसे बताया मैंने कि सो रहा है राम"];
+var word3_9=["i.बताया मैंने उसे कि राम सो रहा है"];
+var word3_10=["j.बताया मैंने उसे कि सो रहा है राम"];
+var word3_11=["k.बताया उसे मैंने कि राम सो रहा है"];
+var word3_12=["l.बताया उसे मैंने कि सो रहा है राम"];
+var word4_1=["a.राम खाकर सोया"];
+var word4_2=["b.खाकर राम सोया"];
+var word4_3=["c.राम सोया खाकर"];
+var word4_4=["d.खाकर सोया राम"];
+var word4_5=["e.सोया राम खाकर"];
+var word4_6=["f.सोया खाकर राम"];
+var word5_1=["a.बिल्लियों को मारकर कुत्ता सो गया"];	
+var word5_2=["b.मारकर बिल्लियों को कुत्ता सो गया"];	 
+var word5_3=["c.बिल्लियों को मारकर सो गया कुत्ता"];	
+var word5_4=["d.मारकर बिल्लियों को सो गया कुत्ता"];	
+var word5_5=["e.कुत्ता सो गय बिल्लियों को मारकर"];	
+var word5_6=["f.कुत्ता सो गया मारकर बिल्लियों को"];	
+var word5_7=["g.सो गया कुत्ता बिल्लियों को मारकर"];
+var word5_8=["h.सो गया कुत्ता मारकर बिल्लियों को"];
+var word6_1=["a.एक लाल किताब वहाँ है"];
+var word6_2=["b.एक लाल किताब है वहाँ"];
+var word6_3=["c.वहाँ है एक लाल किताब"];
+var word6_4=["d.है वहाँ एक लाल किताब"];
+var word7_1=["a.एक बड़ी सी किताब वहाँ है"];	
+var word7_2=["b.एक बड़ी सी किताब है वहाँ"];
+var word7_3=["c.बड़ी सी एक किताब वहाँ है"];
+var word7_4=["d. बड़ी सी एक किताब है वहाँ"];
+var word7_5=["e.वहाँ है एक बड़ी सी किताब"];
+var word7_6=["f.वहाँ है बड़ी सी एक किताब"];
+var word7_7=["g.है वहाँ एक बड़ी सी किताब"];
+var word7_8=["h.है वहाँ बड़ी सी एक किताब"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
